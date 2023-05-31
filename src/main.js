93bfc1c4ffd70677ae08f3bbbfabb0dc0eb81768
Herenyon <rikitur17@gmail.com>
2023-05-31 21:31:08 +0200

    importato font awesome, messo lente in searchbar, prezzo position absolute su gioco flaggato

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import { createApp } from 'vue';
-
 import './assets/scss/main.scss';
 import * as bootstrap from 'bootstrap';
 import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+library.add(faUserSecret, faSearch);
 
-createApp(App).mount('#app')
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
--- src/partials/FlagGame.vue
+++ src/partials/FlagGame.vue
@@ -10,8 +10,26 @@ export default {
         <div class="container py-5">
             <h1>Giochi</h1>
             <div class="d-flex bg-div p-4">
-                <div>
+                <div class="image-container">
                     <img src="https://cdn.wallpapersafari.com/87/92/1Nd4g3.png" alt="">
+                    <div>
+
+                    </div>
+                    <div class="price d-flex">
+                        <div class="d-flex">
+                            <div class="sconto h-100 d-flex align-items-center">
+                                <span class="sconto-span">50%</span>
+                            </div>
+                            <div class="container-prezzo">
+                                <div class="prezzo-pieno text-decoration-line-through">
+                                    29.99
+                                </div>
+                                <div class="prezzo-scontato">
+                                    10.99
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div class="mx-3">
                     <h2>Call of Duty: Black Ops III</h2>
@@ -34,13 +52,56 @@ export default {
 section {
     padding: 0 18.75rem;
     color: white;
-    .container{
+
+    .container {
         background-image: url(https://cdn.wallpapersafari.com/87/92/1Nd4g3.png);
         background-size: cover;
         box-shadow: inset 0px 0px 50px 50px #0f1014;
-        .bg-div{
+
+        .image-container {
+
+            position: relative;
+
+            .price {
+                span {
+                    padding: 0 10px;
+                    background-color: #38404B;
+                    cursor: pointer;
+                    position: absolute;
+                    bottom: 1px;
+                    left: 134px;
+                }
+
+                .sconto-span {
+                    background-color: #4C6D16;
+                    cursor: inherit;
+                    font-size: 28px;
+                    color: #BDEC48;
+                }
+
+                .container-prezzo {
+                    background-color: #34474d;
+                    font-size: 14px;
+                    padding: 0 .3125rem;
+                    position: absolute;
+                    left: 207px;
+                    bottom: 1px;
+
+                    .prezzo-scontato {
+                        color: #BDEC48;
+
+                    }
+                }
+
+
+            }
+        }
+
+
+        .bg-div {
             background: rgba($color: #000000, $alpha: 0.7);
-            .badge{
+
+            .badge {
                 background: rgba($color: #000000, $alpha: 0.4);
                 padding: 7px 10px;
             }

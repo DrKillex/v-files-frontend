6daacfae687fe9cbfcc22bde4335fae1e84912ea
Herenyon <rikitur17@gmail.com>
2023-05-31 17:10:15 +0200

    cominciato parte gioco falaggato

--- src/partials/FlagGame.vue
+++ src/partials/FlagGame.vue
@@ -6,7 +6,25 @@ export default {
 </script>
 
 <template>
-    <h1>Giochi</h1>
+    <section>
+        <h1>Giochi</h1>
+
+        <div class="container d-flex">
+            <div>
+                <img src="https://cdn.wallpapersafari.com/87/92/1Nd4g3.png" alt="">
+            </div>
+            <div class="mx-3">
+                <h2>Call of Duty: Black Ops III</h2>
+                <h4>
+                    Data di rilascio: 6 nov 2015
+                </h4>
+                <div class="genre">
+                    Multigiocatore
+                </div>
+            </div>
+
+        </div>
+    </section>
 </template>
 <style lang="scss" scoped>
 @import '../assets/scss/_partial/flag';
